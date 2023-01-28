a9798dc73cee55089aced21f44a7cbc02b708470
daniel-georgiev-dgmfb <daniel.georgiev73@gmail.com>
2023-01-28 20:28:15 +0200

    extending characters set

--- ParallelLibrary/Program.cs
+++ ParallelLibrary/Program.cs
@@ -10,7 +10,7 @@ public class Example
     {
         var l = new List<string>();
         var stopwatch = new Stopwatch();
-        var source = new char[62] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        var source = new char[70] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '.', '/', '~', '#', '<', '>', '?' };
         //var source = new char[16] { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'a', 'b', 'c', 'd', 'e', 'f' };
         var salt = new byte[4];
         var md5 = MD5.Create();
